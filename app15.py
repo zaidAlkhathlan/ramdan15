159a042da281d599a6d71f0711c9cf6507619aba
zaidAlkhathlan <444001475@sm.imamu.edu.sa>
2025-03-07 19:07:10 +0300

    Day 7

--- app15.py
+++ app15.py
@@ -30,8 +30,8 @@ st.markdown(rtl_css, unsafe_allow_html=True)
 def can_show_riddle():
     local_tz = pytz.timezone("Asia/Riyadh")
     now = datetime.datetime.now(local_tz)
-    start_time = now.replace(hour=19, minute=7, second=0, microsecond=0)  
-    end_time = now.replace(hour=19, minute=10, second=0, microsecond=0)  
+    start_time = now.replace(hour=19, minute=15, second=0, microsecond=0)  
+    end_time = now.replace(hour=19, minute=20, second=0, microsecond=0)  
     return start_time <= now <= end_time
 
 ##############################
