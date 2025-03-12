2c8c7f081ffdb4925eda7f52a36e750e650a9f0f
zaidAlkhathlan <444001475@sm.imamu.edu.sa>
2025-03-12 18:08:30 +0300

    Day 12

--- app15.py
+++ app15.py
@@ -30,8 +30,8 @@ st.markdown(rtl_css, unsafe_allow_html=True)
 def can_show_riddle():
     local_tz = pytz.timezone("Asia/Riyadh")
     now = datetime.datetime.now(local_tz)
-    start_time = now.replace(hour=19, minute=5, second=0, microsecond=0)  
-    end_time = now.replace(hour=19, minute=10, second=0, microsecond=0)  
+    start_time = now.replace(hour=18, minute=20, second=0, microsecond=0)  
+    end_time = now.replace(hour=18, minute=25, second=0, microsecond=0)  
     return start_time <= now <= end_time
 
 ##############################
@@ -48,9 +48,9 @@ db = firestore.client()
 ##############################
 # 🎯 Every day, update this section with a new riddle before uploading
 RIDDLE = {
-    "question": "من هو النبي الذي امن به جميع قومه؟",
-    "options": [" موسى عليه السلام", " عيسى عليه السلام", " يونس عليه السلام", " نوح عليه السلام"],
-    "answer": " يونس عليه السلام"
+    "question": "ما هو البحر الاكثر دفئا في العالم؟",
+    "options": ["البحر الأبيض المتوسط", "بحر العرب", "البحر الميت", "البحر الأحمر"],
+    "answer": "البحر الأحمر"
 }
 
 ##############################
