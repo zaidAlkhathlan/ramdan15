ce650168344dfb594c3628e6e1084b54494b58e9
zaidAlkhathlan <444001475@sm.imamu.edu.sa>
2025-03-16 18:37:19 +0300

    Day 16

--- app15.py
+++ app15.py
@@ -30,8 +30,8 @@ st.markdown(rtl_css, unsafe_allow_html=True)
 def can_show_riddle():
     local_tz = pytz.timezone("Asia/Riyadh")
     now = datetime.datetime.now(local_tz)
-    start_time = now.replace(hour=18, minute=24, second=0, microsecond=0)  
-    end_time = now.replace(hour=18, minute=28, second=0, microsecond=0)  
+    start_time = now.replace(hour=18, minute=40, second=0, microsecond=0)  
+    end_time = now.replace(hour=18, minute=45, second=0, microsecond=0)  
     return start_time <= now <= end_time
 
 ##############################
@@ -48,8 +48,12 @@ db = firestore.client()
 ##############################
 # 🎯 Every day, update this section with a new riddle before uploading
 RIDDLE = {
-    
+    "question": "أي مما يلي هو وحدة قياس المقاومة الكهربائية؟",
+     "options": ["الأمبير", "الأوم", "الفولت", "الواط"],
+     "answer": "الأوم"
+
 }
+
 
 ##############################
 #      USER AUTH SECTION     #
